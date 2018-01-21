9e3496fdc7da8966015b7ef64266f079b5ded40c
ruoshuiwyl <ruoshuiwyl@163.com>
2018-01-21 23:34:37 +0800

    add skiplist

--- skiplist.h
+++ skiplist.h
@@ -5,12 +5,33 @@
 #ifndef BASE_SKIPLIST_H
 #define BASE_SKIPLIST_H
 
+#include <random>
+
 namespace  gamtools {
     class Arena;
+    template < typename  Key, typename Comparator>
+    class SkipList {
+    private :
+        struct Node;
 
-    class Skiplist {
+    public:
+        explicit SkipList(Comparator cmp, Arena* arena);
 
+
+    private:
+        Comparator const compare_;
+        Arena *const arena_;
+        Node *const head_;
+        std::atomic<void *> max_height_;
+        std::uniform_int_distribution<int> rnd_;
     };
+
+
+    template < typename  Key, typename Comparator>
+    SkipList::SkipList(Comparator cmp, Arena *arena) : {
+
+    }
+
 }
 
 #endif //BASE_SKIPLIST_H
